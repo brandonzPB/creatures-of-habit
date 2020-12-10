c0effa0a7368869bb0225b3bec88d95079bf1ebd
Brandon Zirulnikoff <zirbrandonweb@gmail.com>
2020-12-10 13:48:54 -0800

    Changed order of middleware functions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import mongoose from 'mongoose';
 import express from 'express';
 import session from 'express-session';
 import bodyParser from 'body-parser';
-
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -27,9 +26,9 @@ db.once('open', function() {
 });
 
 // change to build for production?
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
-app.use(cors());
 app.use(session({ secret: 'chunkypb', resave: false, saveUninitialized: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
