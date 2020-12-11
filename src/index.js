80d1130cc5ee700a8d49117b057a9b27f47b2bff
Brandon Zirulnikoff <zirbrandonweb@gmail.com>
2020-12-11 13:38:26 -0800

    Server only; removed build folder

--- src/index.js
+++ src/index.js
@@ -32,13 +32,13 @@ db.once('open', function() {
 
 app.use(cors());
 
-if (ck.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+// if (ck.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
+//   app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+//   });
+// }
 
 // app.use(express.static(path.join(__dirname, '../client/build')));
 
