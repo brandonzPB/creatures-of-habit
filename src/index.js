7e3308e4bf4fa43a8f682df39857158a3c82c0b0
Brandon Zirulnikoff <zirbrandonweb@gmail.com>
2020-12-11 17:01:50 -0800

    Cleaned code

--- src/index.js
+++ src/index.js
@@ -44,16 +44,6 @@ db.once('open', function() {
 
 app.use(cors());
 
-// if (ck.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../build')));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../build', 'index.html'));
-//   });
-// }
-
-// app.use(express.static(path.join(__dirname, '../client/build')));
-
 app.use(session({ 
   secret: ck.SESSION_SECRET, 
   resave: false, 
