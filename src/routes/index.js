fbb3e2ad9504f38d3321f40c230de3bd1dcf8673
Brandon Zirulnikoff <zirbrandonweb@gmail.com>
2020-12-13 12:56:52 -0800

    Added console logs in index for testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
+  console.log('NODE_ENV', process.env.NODE_ENV);
+  console.log('hostname', req.hostname);
+  console.log('url', req.url);
+  console.log('header', req.header('x-forwarded-proto'));
   res.redirect('/dashboard');
 });
 
